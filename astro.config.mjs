f1c34a91a5a4ea8ed3fecaa8e779b0269725d903
Klevert Opee <klevertope@gmail.com>
2023-05-15 21:48:36 +0300

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://magazineklert.vercel.app/',
-  integrations: [tailwind(), sitemap(), compressor()]
+  integrations: [tailwind(), compressor()]
 });
