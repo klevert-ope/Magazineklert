41466cce9b4dced99b751bcb4c27e8c4f5b3c353
Klevert Opee <klevertope@gmail.com>
2023-06-11 16:00:39 +0300

    gsap

---
+++ src/js/blog.js
@@ -0,0 +1,46 @@
+import { gsap } from "gsap";
+
+// Header motion
+gsap.fromTo(
+  ".daily",
+  { opacity: 0, y: -10 },
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
+);
+gsap.fromTo(
+  ".pop",
+  { opacity: 0, x: -10 },
+  { opacity: 1, x: 0, duration: 2, ease: "power4.in" }
+);
+gsap.fromTo(
+  ".magazine",
+  { opacity: 0, y: 10 },
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
+);
+gsap.fromTo(
+  ".est",
+  { opacity: 0, y: -10 },
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
+);
+gsap.fromTo(
+  ".sidedrawer",
+  { opacity: 0, y: 20 },
+  { opacity: 1, y: 0, duration: 2.8, ease: "power4.in" }
+);
+
+gsap.fromTo(
+  ".blogtitle",
+  { opacity: 0, y: -20 },
+  { opacity: 1, y: 0, duration: 2.5, ease: "circ.in" }
+);
+
+// reveal elements
+gsap.fromTo(
+  ".reveal",
+  { x: -20, opacity: 0 },
+  {
+    x: 0,
+    duration: 2.8,
+    opacity: 1,
+    ease: "circ.in",
+  }
+);
--- src/js/index.js
+++ src/js/index.js
@@ -1,81 +1,70 @@
 import { gsap } from "gsap";
-import { ExpoScaleEase, RoughEase, SlowMo } from "gsap/EasePack";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
-gsap.registerPlugin(
-  ScrollTrigger,
-  ScrollToPlugin,
-  ExpoScaleEase,
-  RoughEase,
-  SlowMo
-);
+gsap.registerPlugin(ScrollTrigger);
 
 // Header motion
 gsap.fromTo(
   ".daily",
   { opacity: 0, y: -10 },
-  { opacity: 1, y: 0, duration: 3.5, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
 );
 gsap.fromTo(
   ".pop",
   { opacity: 0, x: -10 },
-  { opacity: 1, x: 0, duration: 3, ease: "power2.in" }
+  { opacity: 1, x: 0, duration: 2, ease: "power4.in" }
 );
 gsap.fromTo(
   ".magazine",
   { opacity: 0, y: 10 },
-  { opacity: 1, y: 0, duration: 3.5, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
 );
 gsap.fromTo(
   ".est",
   { opacity: 0, y: -10 },
-  { opacity: 1, y: 0, duration: 3.5, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
 );
 gsap.fromTo(
   ".sidedrawer",
   { opacity: 0, y: 20 },
-  { opacity: 1, y: 0, duration: 3.8, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 2.8, ease: "power4.in" }
 );
 
 // Intro text motion
 gsap.fromTo(
   ".introtexth1",
   { opacity: 0, y: 30 },
-  { opacity: 1, y: 0, duration: 3.5, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 2.5, ease: "power4.in" }
 );
 gsap.fromTo(
   ".introtexth2",
   { opacity: 0, y: 40 },
-  { opacity: 1, y: 0, duration: 3.8, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 3.8, ease: "power4.in" }
 );
 
 //showcase motion select the blog card element
 const blogCard = document.querySelector(".blog-cardscale");
 
-gsap.fromTo(
-  ".blog-cardscale",
-  { opacity: 0, y: 20 },
-  { opacity: 1, y: 0, duration: 3.8, ease: "power2.in" }
-);
+gsap.fromTo(".blog-cardscale", { opacity: 0 }, { opacity: 1 });
 // set the initial scale of the blog card to 0.7
 gsap.set(blogCard, { scale: 0.7 });
 
 // animate the scale of the blog card to 1 when it reaches the top 80% of the viewport
 gsap.to(blogCard, {
   scale: 1,
-  duration: 3,
+  duration: 2.5,
   ease: "steps(3)",
   scrollTrigger: {
     trigger: blogCard,
-    start: "top 85%",
+    start: "top 90%",
+    end: "top 20%",
   },
 });
 
 gsap.fromTo(
   "#fastB",
   { opacity: 0, y: 20 },
-  { opacity: 1, y: 0, duration: 3.8, ease: "power2.in" }
+  { opacity: 1, y: 0, duration: 2.8, ease: "power4.in" }
 );
 
 // select elements to reveal
@@ -90,7 +79,7 @@ elements.forEach((el) => {
       y: 0,
       duration: 1,
       opacity: 1,
-      ease: "power2.in",
+      ease: "power4.in",
       scrollTrigger: {
         trigger: el,
         start: "top 90%",
